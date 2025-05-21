242dcbc08c739155d87b52967b6033b1d7f52e81
Henriques <henriquessaete26@gmail.com>
2025-05-21 10:30:46 +0200

    01.soma-dois-numeros

---
+++ aula-2/01.operadores-artmeticos/01.soma-dois-numeros.js
@@ -0,0 +1,11 @@
+// 1. Calcule a soma de dois números
+
+
+var primeiro = Number(prompt('Digite o primeiro numero'));
+var segundo = Number(prompt('Digite o segundo numero'));
+var soma = primeiro + segundo;
+
+
+alert(`A soma de ${primeiro} + ${segundo} = ${soma}`);
+
+
