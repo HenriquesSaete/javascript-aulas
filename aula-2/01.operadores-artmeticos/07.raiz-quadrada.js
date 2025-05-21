385373fd10a84bab6e7737ac4914498d9fc8581c
Henriques <henriquessaete26@gmail.com>
2025-05-21 17:03:03 +0200

    07.Raiz-quadrada

---
+++ aula-2/01.operadores-artmeticos/07.raiz-quadrada.js
@@ -0,0 +1,6 @@
+// 7. Calcule a raiz quadrada de um número.
+
+var numero = Number(prompt('Digite um numero'));
+var raiz = Math.sqrt(numero);
+
+alert(`A raiz quadrada de ${numero} é igual a ${raiz}`);
