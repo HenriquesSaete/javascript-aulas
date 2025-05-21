19f216cab52836654d89fc4fa631aeb4f7c1105a
Henriques <henriquessaete26@gmail.com>
2025-05-21 15:54:19 +0200

    05.Resto-da-divisão

---
+++ aula-2/01.operadores-artmeticos/05.resto-da-divisao.js
@@ -0,0 +1,5 @@
+var resto1 = Number(prompt('Digite o primeiro numero'));
+var resto2 = Number(prompt('Digite o segundo numero'));
+var soma = resto1 % resto2;
+
+alert(`O resto da divisão de ${resto1} % ${resto2} = ${soma}`);
