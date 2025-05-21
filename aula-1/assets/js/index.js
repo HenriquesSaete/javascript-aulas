7752fac9f895a4ef1bc4b9f7957cf3aafd485877
Henriques <henriquessaete26@gmail.com>
2025-05-21 09:00:03 +0200

    Aula 1

---
+++ aula-1/assets/js/index.js
@@ -0,0 +1,26 @@
+// comentario de uma so linha 
+
+/*
+comentario de multiplas linhas 
+*/
+
+//window.alert('Aula 1')
+
+/*
+var nome = Number( prompt('Digite um numero'))
+var resultado = nome / 3
+window.alert(`o que voce digitou e isso. ${resultado}`)
+*/
+
+/*
+var nome = prompt('Digite seu nome')
+var nota_1 = Number(prompt('Primeira nota'))
+var nota_2 = Number (prompt('Segunda nota'))
+var media = (nota_1 + nota_2)  / 2
+window.alert(`Henriques sua media foi ${media}` )
+*/
+
+var nome = prompt ('Digite o seu nome')
+var idade = Number(prompt('Digite a sua idade'))
+var resultado = idade * 365
+window.alert( `${nome} Voce tem 20 anos, entao voce ja viveu ${resultado} dias. `) 
