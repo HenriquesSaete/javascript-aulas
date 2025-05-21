f792958da4f2d95b7f35d203f3c28a5ca0ee7297
Henriques <henriquessaete26@gmail.com>
2025-05-21 17:25:00 +0200

    09.valor-absluto

---
+++ aula-2/01.operadores-artmeticos/09.valor-absoluto.js
@@ -0,0 +1,6 @@
+// 9. Calcule o valor absoluto de um número.
+
+var numero = Number(prompt('Digite um numero'));
+var valorAbsoluto = Math.abs(numero);
+
+alert(`O valor absoluto de ${numero} é de ${valorAbsoluto}`);
