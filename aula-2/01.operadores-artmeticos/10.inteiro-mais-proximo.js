40ba0cc059464f0c7a0e9ef10a7560f04db5e11b
Henriques <henriquessaete26@gmail.com>
2025-05-21 17:43:57 +0200

    10.numero-ineiro-mais-proximo

---
+++ aula-2/01.operadores-artmeticos/10.inteiro-mais-proximo.js
@@ -0,0 +1,6 @@
+// 10. Arredonde um número para o inteiro mais próximo.
+
+var numero = Number(prompt('Digite um numero'));
+var inteiroProximo = Math.round(numero);
+
+alert(`O numero inteiro mais proximo ${numero} é de ${inteiroProximo}`);
