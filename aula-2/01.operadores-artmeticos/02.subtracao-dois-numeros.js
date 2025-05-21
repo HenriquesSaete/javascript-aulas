dc1a494c04f4f44ea6b54d8c1a13928f9acb345c
Henriques <henriquessaete26@gmail.com>
2025-05-21 10:49:07 +0200

    02.subtracao-dois-numeros

---
+++ aula-2/01.operadores-artmeticos/02.subtracao-dois-numeros.js
@@ -0,0 +1,5 @@
+var primeiro = Number(prompt('Digite o primeiro numero'));
+var segundo = Number(prompt('Digite o segundo numero'));
+var soma = primeiro - segundo; 
+
+alert (`A subtracao de ${primeiro} - ${segundo} = ${soma}`);
