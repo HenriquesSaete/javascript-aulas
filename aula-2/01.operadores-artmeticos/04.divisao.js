90a6bea90daef14379c99c19bd3bb4390912eb49
Henriques <henriquessaete26@gmail.com>
2025-05-21 15:29:29 +0200

    04.Divisão

---
+++ aula-2/01.operadores-artmeticos/04.divisao.js
@@ -0,0 +1,7 @@
+// 4. Divida um número por outro.
+
+var divisao1 = Number(prompt('Digite o primeiro numero'));
+var divisao2 = Number(prompt('Digite o segundo numero numero'));
+var soma =  divisao1 / divisao2;
+
+alert(`A divisao de ${divisao1} / ${divisao2} = ${soma}`);
