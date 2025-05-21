1a1a2519b2d99567547edabb367ae4ea83a4d1fd
Henriques <henriquessaete26@gmail.com>
2025-05-21 15:18:36 +0200

    03.multiplicação-dois-numeros

---
+++ aula-2/01.operadores-artmeticos/03.multiplicacao-dois-numeros.js
@@ -0,0 +1,7 @@
+// 3. Multiplique dois números
+
+var numero1 = Number(prompt('Digite o primeiro numero'));
+var numero2 = Number(prompt('Digite o segundo numero'));
+var soma = numero1 * numero2;
+
+alert(`A multiplicaçao de ${numero1} * ${numero2} = ${soma}`);
